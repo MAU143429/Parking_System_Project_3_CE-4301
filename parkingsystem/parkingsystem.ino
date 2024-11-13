162cffc7db8e30cad15ebed0c9224c8cf73af5d6
MAU143429 <mau141226@estudiantec.cr>
2024-11-12 21:55:26 -0600

    Final Commit

--- parkingsystem/parkingsystem.ino
+++ parkingsystem/parkingsystem.ino
@@ -14,11 +14,12 @@ int umbralDistancia = 10;                // Umbral de distancia (en cm) para act
 Servo servoMotor;  // Crear objeto servo
 
 // Definición de segmentos para cada dígito
+//A   B  C  D  E  F  G  
 const bool numeros[10][7] = {
   {0, 0, 0, 0, 0, 0, 1}, // 0
   {1, 0, 0, 1, 1, 1, 1}, // 1
   {0, 0, 1, 0, 0, 1, 0}, // 2
-  {0, 0, 0, 0, 1, 1, 0}, // 3
+  {0, 0, 0, 1, 0, 1, 0}, // 3
   {1, 0, 0, 1, 1, 0, 0}, // 4
   {0, 1, 0, 0, 1, 0, 0}, // 5
   {0, 1, 0, 0, 0, 0, 0}, // 6
